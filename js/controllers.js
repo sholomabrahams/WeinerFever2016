600a9f2bb194706521dddf0f190f5a2afecce531
sholomabrahams <sabrahams2018@btfiloh.org>
2016-06-06 17:18:44 -0400

    More Changes
    Still no integration of Firebase properly

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 var weinerControllers = angular.module('weinerControllers', []);
 
 /* FACTORIES */
-weinerControllers.run(['$rootScope', function($rootScope){
+weinerControllers.run(['$rootScope', '$http', function($rootScope, $http){
 	//Set body class no-touch if not touchscreen
 	if (!('ontouchstart' in window || navigator.maxTouchPoints)) {
 		$rootScope.touch = "no-touch";
@@ -18,16 +18,35 @@ weinerControllers.run(['$rootScope', function($rootScope){
 
 	$rootScope.menuOpen = "closed";
 
+	var data;
+	var current;
+	var games;
+	$http.get('/ng/weinerfever.json').then(function (response) {
+		data = response.data;
+		games = data.games;
+		//console.log(data);
+		for (game in games) {
+			console.log(game.home);
+			if (game.home == "Beth Tfiloh" || game.home == "Boyar") {
+				console.log(data.teams[game.home]);
+				data.games[game].hPath = data.teams[game.home].path;
+			} else {
+				data.games[game].hPath = "Ramaz";
+			}
+		}
+		$rootScope.games = games;
+		$rootScope.teams = data.teams;
+	});
+
 	//Makes base firebase ref available as $rootScope.ref
 //	$rootScope.ref = new Firebase("https://weinerfever.firebaseio.com/");
 }]);
 
 /*CONTROLLERS*/
-weinerControllers.controller('home', ['$scope', '$firebaseObject', '$rootScope', function($scope, $firebaseObject, $rootScope) {
+weinerControllers.controller('home', ['$scope', '$firebaseObject', '$rootScope', '$http', function($scope, $firebaseObject, $rootScope, $http) {
 //	var obj = new $firebaseObject($rootScope.ref.child('games').child('b01').child('time'));
 	//obj.$loaded().then(function () {console.log(obj)});
 //	$scope.games = obj;
-	$scope.games = {'$value': '3:30PM'};
 	/*var unwatch = obj.$watch(function () {
 		if (!($scope.games.$value)) {
 			$scope.show = "none";
